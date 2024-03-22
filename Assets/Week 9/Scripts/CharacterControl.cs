97719d1fce044148f197335463730e1a3c55d83a
zhalingg <zhaolingge82@gmail.com>
2024-03-21 20:16:23 -0400

    Task 4a): make a world-space UI done.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public Villager[] characters;
     public TMPro.TextMeshProUGUI currentSelection;
+    public TMP_Dropdown dropdown;
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -23,5 +27,10 @@ public class CharacterControl : MonoBehaviour
         {
             currentSelection.text = SelectedVillager.GetType().ToString();
         }
+    }
+
+    public void DropDownHasChanged(int value)
+    {
+        SetSelectedVillager(characters[value]);
     }
 }
