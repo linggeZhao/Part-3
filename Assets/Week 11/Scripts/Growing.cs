7f411f6ac0616f3a98f3c5894f7394d1fe542f66
Lingge <zhaolingge82@gmail.com>
2024-03-29 18:48:21 -0400

    import

---
+++ Assets/Week 11/Scripts/Growing.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Growing : MonoBehaviour
+{
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject circle;
+    public TextMeshProUGUI squareTMP;
+    public TextMeshProUGUI triangleTMP;
+    public TextMeshProUGUI circleTMP;
+    public TextMeshProUGUI crTMP;
+    public int running;
+    Coroutine coroutine;
+
+    void Start()
+    {
+        Square();
+        Triangle();
+        Circle();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Square()
+    {
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            square.transform.localScale = scale;
+            squareTMP.text = "Square: " + scale;
+        }
+    }
+    void Triangle()
+    {
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            triangle.transform.localScale = scale;
+            triangleTMP.text = "Triangle: " + scale;
+        }
+    }
+    void Circle()
+    {
+
+        float size = 0;
+        while (size < 5)
+        {
+            size += Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+        }
+        while (size > 0)
+        {
+            size -= Time.deltaTime;
+            Vector3 scale = new Vector3(size, size, size);
+            circle.transform.localScale = scale;
+            circleTMP.text = "Cirlce: " + scale;
+        }
+    }
+}
